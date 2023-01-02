30a22108135fe9b367ad8250c8f13e416accf17e
TiagoMRib <tiago18.2002@gmail.com>
2023-01-02 16:39:22 +0000

    Update alt_game.pl

--- alt_game.pl
+++ alt_game.pl
@@ -38,14 +38,12 @@ initialization:-
     write('3-Intelligent Bot'), nl,
     read(Option1),
     type_of_player(Option1, Player1),
-    write(Player1), 
     write('Player 2 (black):'), nl,
     write('1-Human'), nl,
     write('2-Easy Bot'), nl,
     write('3-Intelligent Bot'), nl,
     read(Option2),
     type_of_player(Option2, Player2),
-    write(Player2),
     define_players(Player1, Player2),
 
     play_game(Board, Player1, white).
@@ -92,6 +90,7 @@ play_game(Board, human, Color) :-
     n_pieces(black, Nblack),
     write('Number of white pieces left: '), write(Nwhite),nl,
     write('Number of black pieces left: '), write(Nblack),nl,
+    write('Your turn: '), write(Color), nl, 
     write('Select 1 to place a piece, 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
     read(Option), nl,
     move(Option, Board, Color, NewBoard), 
@@ -109,6 +108,19 @@ play_game(Board, human, Color) :-
      ;   play_game(NewBoard, OtherPlayer, NextColor)
     ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%  PLACEMET SECTION
+place_move(Board, Color, Pos, NewBoard):-
+    empty(Board,Pos), 
+    place(Board, Pos, Color, NewBoard),
+    retract_pieces(Color).
+
+place_move(Board, Color, Pos, NewBoard):-
+    \+empty(Board, Pos),    % already checks if its inside board
+    write('You cant place it there.'), nl,
+    write('Select another move'), nl,
+    write('Select 1 to place a piece, 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
+    read(Option), nl,
+    move(Option, Board, Color, NewBoard).
 
 
 move(1, Board, Color, NewBoard) :-
@@ -118,10 +130,7 @@ move(1, Board, Color, NewBoard) :-
     read(Pos), nl,
     atom_chars(Pos, InputList),
     convert_letter_to_number(InputList,NewPos),
-    empty(Board, NewPos),
-    inside_board(Board, NewPos),
-    place(Board, NewPos, Color, NewBoard),
-    retract_pieces(Color).
+    place_move(Board, Color, NewPos, NewBoard).
 
 move(1, Board, Color, NewBoard) :-
     n_pieces(Color, 0),
@@ -129,40 +138,64 @@ move(1, Board, Color, NewBoard) :-
     write('Select another move'), nl,
     write('Select 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
     read(Option), nl,
-    move(Option, Board, Player, NewBoard).
+    move(Option, Board, Color, NewBoard).
 
-move(1, Board, Color, NewBoard) :-
-    n_pieces(Color, Np),
-    Np > 0, nl,
-    write('Select the position you want to place the piece:'), nl,
-    read(Pos), nl,
-    atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,NewPos),
-    \+empty(Board, NewPos),    % already checks if its inside board
-    write('You cant place it there.'), nl,
-    write('Select another move'), nl,
-    write('Select 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%   MOVEMENT SECTION
+move_move(Board, Color, Pos, Dir, NewBoard):-
+    move_piece(Board, Pos, Dir, Color, NewBoard), nl.
+
+move_move(Board, Color, Pos, Dir, NewBoard):-
+    \+move_piece(Board, Pos, Dir, Color, NewBoard), 
+     write('Invalid move!'), nl,
+     write('Select 1 to place a piece, 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
     read(Option), nl,
-    move(Option, Board, Player, NewBoard).
+    move(Option, Board, Color, NewBoard).
 
-
-
-%%%%%%%%%%% TO BE SEEN
-
-
-move(2, Board, Color, NewBoard) :-
-    write('Select the position of the piece you want to move:'), nl,
-    read(Pos),nl,
-    atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,NewPos), 
+select_move_move(Board, Color, Pos, NewBoard):-
+    is_mine(Color, Simbol),
+    pos(Board, Pos, Simbol),
     write('Choose a direction to move to:'), nl,
     write('1. Down'), nl,
     write('2. Up'), nl,
     write('3. Right'), nl,
     write('4. Left'), nl,
     read(Dir),nl,
-    move_piece(Board, NewPos, Dir, Color, NewBoard), nl.
+    move_move(Board, Color, Pos, Dir, NewBoard).
 
+select_move_move(Board, Color, Pos, NewBoard):-
+    is_mine(Color, Simbol),
+    \+pos(Board, Pos, Simbol),
+    write('That piece isnt your piece'), nl,
+    write('Select 1 to place a piece, 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
+    read(Option), nl,
+    move(Option, Board, Color, NewBoard).
+
+move(2, Board, Color, NewBoard) :-
+    write('Select the position of the piece you want to move:'), nl,
+    read(Pos),nl,
+    atom_chars(Pos, InputList),
+    convert_letter_to_number(InputList,NewPos),
+    select_move_move(Board, Color, NewPos, NewBoard).
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%% CAPTURE SECTION
+
+execute_extraCapture(Board, Color, (X,Y), NewBoard):-
+    enemy(Board, (X,Y), Color),
+    change_board_element(Board, X, Y, 0, NewBoard),
+    opponent(Color, OtherColor),
+    retract_pieces(OtherColor),
+    display_board(NewBoard).
+
+execute_extraCapture(Board, Color, (X,Y), NewBoard):-
+    \+enemy(Board, (X,Y), Color),
+    write('Thats not an enemy'),nl,
+    extraCapture(Board, Color, NewBoard).
 
 extraCapture(Board, Color, Board):-
     opponent(Color, OtherColor),
@@ -173,104 +206,79 @@ extraCapture(Board, Color, Board):-
 
 
 extraCapture(Board, Color, NewBoard):-
+    opponent(Color, OtherColor),
+    is_mine(OtherColor, Simbol),
+    pos(Board, _, Simbol),
     write('Choose now the extra capture:'),
     read(Pos), nl,
     atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,(X,Y)),
-    enemy(Board, (X,Y), Color),
-    change_board_element(Board, X, Y, 0, NewBoard),
-    opponent(Color, OtherColor),
-    retract_pieces(OtherColor),
-    display_board(NewBoard).
-
-extraCapture(Board, Color, NewBoard):-
-    write('Choose now the extra capture:'), nl,
-    read(Pos), nl,
-    atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,(X,Y)),
-    \+enemy(Board, (X,Y), Color),
-    write('Thats not an enemy'),nl,
-    extraCapture(Board, Color, NewBoard).
+    convert_letter_to_number(InputList,NewPos),
+    execute_extraCapture(Board, Color, NewPos, NewBoard).
 
 
-move(3, Board, Color, NewBoard) :-
-    write('Select the position of the piece you want to use for a capture:'), nl,
-    read(Pos),nl,
-    atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,NewPos), 
-    is_mine(Color, Simbol),
-    \+pos(Board, NewPos, Simbol),
-    write('That piece isnt your piece'), nl,
-    write('Select 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
-    read(Option), nl,
-    move(Option, Board, Player, NewBoard).
-
-
-
-move(3, Board, Color, NewBoard) :-
-    write('Select the position of the piece you want to use for a capture:'), nl,
-    read(Pos),nl,
-    atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,NewPos), 
-    is_mine(Color, Simbol),
-    pos(Board, NewPos, Simbol),
-    write('Choose the direction you will be capturing:'), nl,
-    write('1. Down'), nl,
-    write('2. Up'), nl,
-    write('3. Right'), nl,
-    write('4. Left'), nl,
-    read(Dir),nl,
-    \+move_piece_to_eat(Board, NewPos, FinalPos, Dir, Color, UpdBoard), nl,
-    write('Invalid Move.'), nl,
-    write('Select another move'), nl,
-    write('Select 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
-    read(Option), nl,
-    move(Option, Board, Player, NewBoard).
-
-
-move(3, Board, Color, NewBoard) :-
-    write('Select the position of the piece you want to use for a capture:'), nl,
-    read(Pos),nl,
-    atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,NewPos), 
-    write('Choose the direction you will be capturing:'), nl,
-    write('1. Down'), nl,
-    write('2. Up'), nl,
-    write('3. Right'), nl,
-    write('4. Left'), nl,
-    read(Dir),nl,
-    move_piece_to_eat(Board, NewPos, FinalPos, Dir, Color, UpdBoard), nl,
-    opponent(Color, OtherColor),
-    retract_pieces(OtherColor),
-    display_board(NewBoard),
-    extraCapture(UpdBoard, Color, PostBoard),
-    display_board(PostBoard),
-    \+valid_jump(PostBoard, NewPos, _, _, Color).
-
-move(3, Board, Color, NewBoard) :-
-    write('Select the position of the piece you want to use for a capture:'), nl,
-    read(Pos),nl,
-    atom_chars(Pos, InputList),
-    convert_letter_to_number(InputList,NewPos), 
-    write('Choose the direction you will be capturing:'), nl,
-    write('1. Down'), nl,
-    write('2. Up'), nl,
-    write('3. Right'), nl,
-    write('4. Left'), nl,
-    read(Dir),nl,
-    move_piece_to_eat(Board, NewPos, FinalPos, Dir, Color, UpdBoard), nl,
-    opponent(Color, OtherColor),
-    retract_pieces(OtherColor),
-    display_board(NewBoard),
-    extraCapture(UpdBoard, Color, PostBoard),
-    display_board(PostBoard),
-    valid_jump(PostBoard, NewPos, _, _, Color),
+capture_chain(Board, Color, Pos, NewBoard):-
+    valid_jump(Board, Pos, _, _, Color),
     write('You can do a chain capture'), nl,
-    move(3, PostBoard, Color, NewBoard).
+    display_board(Board),
+    select_capture_move(Board, Color, Pos, NewBoard).
+
+capture_chain(Board, Color, Pos, Board):-
+    \+valid_jump(Board, Pos, _, _, Color).
+
+
+capture_move(Board, Color, Pos, Dir, NewBoard):-
+    move_piece_to_eat(Board, Pos, NewPos, Dir, Color, UpdBoard), nl,
+    display_board(UpdBoard),
+    extraCapture(UpdBoard, Color, OtherBoard),
+    capture_chain(OtherBoard, Color, NewPos, NewBoard).
+    
+
+capture_move(Board, Color, Pos, Dir, NewBoard):-
+    move_piece_to_eat(Board, Pos, NewPos, Dir, Color, UpdBoard), nl,
+    display_board(UpdBoard),
+    extraCapture(UpdBoard, Color, NewBoard),
+    \+valid_jump(NewBoard, NewPos, _, _, Color).
 
 
 
-move(4, Board, NumberWhitePieces, NumberBlackPieces, Type, NewBoard, NewWhitePieces, NewBlackPieces) :-
+capture_move(Board, Color, Pos, Dir, NewBoard):-
+    \+move_piece_to_eat(Board, Pos, _, Dir, Color, _), 
+     write('Invalid move!'), nl,
+     write('Select 1 to place a piece, 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
+    read(Option), nl,
+    move(Option, Board, Color, NewBoard).
+
+select_capture_move(Board, Color, Pos, NewBoard):-
+    is_mine(Color, Simbol),
+    pos(Board, Pos, Simbol),
+    write('Choose the direction you will be capturing:'), nl,
+    write('1. Down'), nl,
+    write('2. Up'), nl,
+    write('3. Right'), nl,
+    write('4. Left'), nl,
+    read(Dir),nl,
+    capture_move(Board, Color, Pos, Dir, NewBoard).
+
+select_capture_move(Board, Color, Pos, NewBoard):-
+    is_mine(Color, Simbol),
+    \+pos(Board, Pos, Simbol),
+    write('That piece isnt your piece'), nl,
+    write('Select 1 to place a piece, 2 to move a piece, 3 to eat a piece and 4 to quit.'), nl,
+    read(Option), nl,
+    move(Option, Board, Color, NewBoard).
+
+
+
+move(3, Board, Color, NewBoard) :-
+    write('Select the position of the piece you want to use for a capture:'), nl,
+    read(Pos),nl,
+    atom_chars(Pos, InputList),
+    convert_letter_to_number(InputList,NewPos), 
+    select_capture_move(Board, Color, NewPos, NewBoard).
+
+
+
+move(4, Board, Color, NewBoard) :-
     write('Game over'), nl,
     break.
 
